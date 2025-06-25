1b234458bdcf7ec013f55984f8cb8ae5c5ed0c8e
hugo <devhugok@gmail.com>
2025-06-25 20:03:15 +0200

    modify path to html page

--- js/script.js
+++ js/script.js
@@ -8,25 +8,25 @@ init(urlProducts);
 /* Récupérer la liste de tous les produits*/
 function init(url) {
   fetch(url)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    displayProducts(value);
-  })
-  .catch(function(err) {
-    window.alert("Échec de la connexion");
-  });
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      displayProducts(value);
+    })
+    .catch(function (err) {
+      window.alert("Échec de la connexion");
+    });
 }
 
 const container = document.getElementById("items");
 
 //Afficher les produits dans HTML
 function displayProducts(products) {
-  for (let i = 0; i < products.length; i++){
-    container.innerHTML += `<a href="./product.html?id=${products[i]._id}">          
+  for (let i = 0; i < products.length; i++) {
+    container.innerHTML += `<a href="./html/product.html?id=${products[i]._id}">          
                               <article>
                                   <img src="${products[i].imageUrl}"" alt="${products[i].altTxt}">
                                   <h3 class="productName">${products[i].name}</h3>
@@ -35,7 +35,3 @@ function displayProducts(products) {
                             </a>`;
   }
 }
-
-
-
-  
